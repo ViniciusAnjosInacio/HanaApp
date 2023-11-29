3428433a0fd546fb40b6b14cf2ff30d622d799a6
Vinicius Inacio <vinianjosinacio@gmail.com>
2023-11-29 14:19:30 +0000

    1° ponto de restauração

--- app/odata_crud/webapp/controller/View1.controller.js
+++ app/odata_crud/webapp/controller/View1.controller.js
@@ -1,15 +1,23 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/m/MessageToast",
+	"sap/m/MessageBox",
+    'sap/ui/model/json/JSONModel'
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller,MessageToast, MessageBox,JSONModel) {
         "use strict";
 
-        return Controller.extend("odatacrud.controller.View1", {
+        var ListController = Controller.extend("sap.m.sample.ListCounter.List",{
+
+        
             onInit: function () {
-                
+                var oModel = new JSONModel(sap.ui.require.toUrl("sap/ui/demo/mock/products.json"));
+			    this.getView().setModel(oModel);
             }
-        });
+    });
+            
+        return Controller;
     });
