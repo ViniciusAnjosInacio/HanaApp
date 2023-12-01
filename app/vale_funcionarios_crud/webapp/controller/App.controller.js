3d3e485e4e2efd5a9c3c08312b8ab2ac124bfb04
Vinicius Inacio <vinianjosinacio@gmail.com>
2023-12-01 16:20:52 +0000

    Combobox genero e não funcionando excel

--- app/vale_funcionarios_crud/webapp/controller/App.controller.js
+++ app/vale_funcionarios_crud/webapp/controller/App.controller.js
@@ -1,5 +1,6 @@
 sap.ui.define(
   [
+    "sap/ui/core/Messaging",
     "sap/ui/core/mvc/Controller",
     "sap/m/MessageToast",
     "sap/m/MessageBox",
@@ -7,40 +8,140 @@ sap.ui.define(
     "sap/ui/model/Filter",
     "sap/ui/model/FilterOperator",
     "sap/ui/model/FilterType",
-    "sap/ui/model/json/JSONModel"
+    "sap/ui/model/json/JSONModel",
+    'sap/ui/core/util/MockServer',
+    'sap/ui/export/library',
+    'sap/ui/export/Spreadsheet',
+    'sap/ui/model/odata/v2/ODataModel'
   ],
-  function (BaseController, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType, JSONModel) {
+  function (Messaging, BaseController, MessageToast, MessageBox, Sorter, Filter, FilterOperator, FilterType, JSONModel,oModel, MockServer, exportLibrary, Spreadsheet, ODataModel) {
     "use strict";
+
+    var Edm = exportLibrary.EdmType;
 
     return BaseController.extend("valefuncionarioscrud.controller.App", {
 
-      onInit: function () {
-        var oMessageManager = sap.ui.getCore().getMessageManager(),
-          oMessageModel = oMessageManager.getMessageModel(),
+ // Configuração do modelo para mensagens e tratamento de erros técnicos
+ 
+      onInit: function () {  
+
+        var oModel = new JSONModel({
+          selectedCountry: "BR",
+          countries: [
+            { key: "BR", text: "Brasil" },
+            { key: "AR", text: "Argentina" },
+            { key: "ME", text: "México" }
+          ],
+
+          genero: [
+            { key: "F", text: "Feminino" },
+            { key: "M", text: "Masculino" }
+        ]
+        });
+
+        var oMessageModel = Messaging.getMessageModel(),
           oMessageModelBinding = oMessageModel.bindList("/", undefined, [],
             new Filter("technical", FilterOperator.EQ, true)),
           oViewModel = new JSONModel({
             busy: false,
             hasUIChanges: false,
-            usernameEmpty: true,
+            usernameEmpty: false,
             order: 0
           });
+
+        // Configuração dos modelos na visão (View)
         this.getView().setModel(oViewModel, "appView");
         this.getView().setModel(oMessageModel, "message");
+        this.getView().setModel(oModel, "model");
 
         oMessageModelBinding.attachChange(this.onMessageBindingChange, this);
         this._bTechnicalErrors = false;
       },
 
-      onCreate : function () {
-        var oList = this.byId("_IDGenTable1"),
+       // Exportação de dados para Excel
+      createColumnConfig: function() {
+        var aCols = [];
+  
+        aCols.push({
+          label: 'ID',
+          type: Edm.Int32,
+          property: 'ID',
+          scale: 0
+        });
+  
+        aCols.push({
+          property: 'Nome',
+          type: Edm.String
+        });
+  
+        aCols.push({
+          property: 'Genero',
+          type: Edm.String
+        });
+  
+        aCols.push({
+          property: 'Estado',
+          type: Edm.String
+        });
+  
+        aCols.push({
+          property: 'Pais',
+          type: Edm.String,
+          scale: 2,
+          delimiter: true
+        });
+  
+        return aCols;
+      },
+  
+      // Exportação dos dados para um arquivo Excel
+      onExport: function() {
+        console.log("Exportação iniciada");
+        var aCols, oRowBinding, oSettings, oSheet, oTable;
+  
+        if (!this._oTable) {
+          this._oTable = this.byId('IDTabelaFuncionario');
+        }
+  
+        oTable = this._oTable;
+        oRowBinding = oTable.getBinding('items');
+        aCols = this.createColumnConfig();
+  
+        oSettings = {
+          workbook: {
+            columns: aCols,
+            hierarchyLevel: 'Level'
+          },
+          dataSource: oRowBinding,
+          fileName: 'tabela.xlsx',
+          worker: false 
+        };
+  
+        oSheet = new Spreadsheet(oSettings);
+        oSheet.build().finally(function() {
+          oSheet.destroy();
+        });
+      },
+  
+      // Ações a serem executadas ao sair da aplicação
+      onExit: function() {
+        this._oMockServer.stop();
+      },
+
+
+      // Operações CRUD (Criar, Ler, Atualizar, Deletar)
+
+      // Criação de um novo item
+      onCreate: function () {
+        var oList = this.byId("IDTabelaFuncionario"),
           oBinding = oList.getBinding("items"),
           oContext = oBinding.create({
+            ID: this._generateNewID()
           });
-  
+
         this._setUIChanges(true);
         this.getView().getModel("appView").setProperty("/usernameEmpty", true);
-  
+
         oList.getItems().some(function (oItem) {
           if (oItem.getBindingContext() === oContext) {
             oItem.focus();
@@ -50,7 +151,59 @@ sap.ui.define(
         });
       },
 
-      onInputChange : function (oEvt) {
+      // Geração de um novo ID
+      _generateNewID: function () {
+        // Obtenha a referência para a tabela
+        var oTable = this.byId("IDTabelaFuncionario");
+      
+        // Obtenha a ligação dos itens da tabela
+        var oBinding = oTable.getBinding("items");
+      
+        // Obtenha a quantidade atual de itens na tabela
+        var iTableLength = oBinding.getLength();
+      
+        // Se a tabela estiver vazia, retorne 1 como o novo ID
+        if (iTableLength === 0) {
+          return 1;
+        }
+      
+        // Obtenha o contexto do último item na tabela
+        var oLastItemContext = oBinding.getContexts()[iTableLength - 1];
+      
+        // Obtenha o valor do ID do último item
+        var iLastItemID = oLastItemContext.getProperty("ID");
+      
+        // Adicione 1 ao ID do último item para obter o novo ID
+        var iNewItemID = iLastItemID + 1;
+      
+        return iNewItemID;
+      },
+
+       // Exclusão de um item
+      onDelete: function () {
+        var oContext,
+          oSelected = this.byId("IDTabelaFuncionario").getSelectedItem(),
+          sUserName;
+
+        if (oSelected) {
+          oContext = oSelected.getBindingContext();
+          sUserName = oContext.getProperty("Nome");
+          oContext.delete().then(function () {
+            MessageToast.show(this._getText("Funcionário deletado", [sUserName]));
+          }.bind(this), function (oError) {
+            this._setUIChanges();
+            if (oError.canceled) {
+              MessageToast.show(this._getText("Funcionário não deletado", [sUserName]));
+              return;
+            }
+            MessageBox.error(oError.message + ": " + sUserName);
+          }.bind(this));
+          this._setUIChanges();
+        }
+      },
+
+      // Manipulação de alterações em campos de entrada
+      onInputChange: function (oEvt) {
         if (oEvt.getParameter("escPressed")) {
           this._setUIChanges();
         } else {
@@ -61,9 +214,9 @@ sap.ui.define(
         }
       },
 
-
+       // Atualização da lista de itens
       onRefresh: function () {
-        var oBinding = this.byId("_IDGenTable1").getBinding("items");
+        var oBinding = this.byId("IDTabelaFuncionario").getBinding("items");
 
         if (oBinding.hasPendingChanges()) {
           MessageBox.error(this._getText("Página não Recarregada"));
@@ -73,42 +226,56 @@ sap.ui.define(
         MessageToast.show(this._getText("Página Recarregada"));
       },
 
-
-
-      onResetChanges : function () {
-        this.byId("_IDGenTable1").getBinding("items").resetChanges();
-        this._bTechnicalErrors = false; 
+      // Reverter alterações não salvas
+      onResetChanges: function () {
+        this.byId("IDTabelaFuncionario").getBinding("items").resetChanges();
+        this._bTechnicalErrors = false;
         this._setUIChanges(false);
       },
 
-      
+      // Salvamento de alterações no servidor
+      onResetDataSource: function () {
+        var oModel = this.getView().getModel(),
+          oOperation = oModel.bindContext("/ResetDataSource(...)");
 
-      onSave : function () {
+        oOperation.execute().then(function () {
+          oModel.refresh();
+          MessageToast.show(this._getText("Dados Restaurados"));
+        }.bind(this), function (oError) {
+          MessageBox.error(oError.message);
+        }
+        );
+      },
+
+      // Salvamento de alterações no servidor
+      onSave: function () {
         var fnSuccess = function () {
           this._setBusy(false);
-          MessageToast.show(this._getText("changesSentMessage"));
+          MessageToast.show(this._getText("Dados Salvos"));
           this._setUIChanges(false);
         }.bind(this),
-  
-         fnError = function (oError) {
-          this._setBusy(false);
-          this._setUIChanges(false);
-          MessageBox.error(oError.message);
-        }.bind(this);
-  
+
+          fnError = function (oError) {
+            this._setBusy(false);
+            this._setUIChanges(false);
+            MessageBox.error(oError.message);
+          }.bind(this);
+
         this._setBusy(true); // Lock UI until submitBatch is resolved.
         this.getView().getModel().submitBatch("peopleGroup").then(fnSuccess, fnError);
         this._bTechnicalErrors = false; // If there were technical errors, a new save resets them.
       },
 
+      // Pesquisa na lista de itens
       onSearch: function () {
         var oView = this.getView(),
           sValue = oView.byId("searchField").getValue(),
           oFilter = new Filter("Nome", FilterOperator.Contains, sValue);
 
-        oView.byId("_IDGenTable1").getBinding("items").filter(oFilter, FilterType.Application);
+        oView.byId("IDTabelaFuncionario").getBinding("items").filter(oFilter, FilterType.Application);
       },
 
+       // Ordenação da lista de itens
       onSort: function () {
         var oView = this.getView(),
           aStates = [undefined, "asc", "desc"],
@@ -120,44 +287,47 @@ sap.ui.define(
         var sOrder = aStates[iOrder];
 
         oView.getModel("appView").setProperty("/order", iOrder);
-        oView.byId("_IDGenTable1").getBinding("items").sort(sOrder && new Sorter("Nome", sOrder === "desc"));
+        oView.byId("IDTabelaFuncionario").getBinding("items").sort(sOrder && new Sorter("Nome", sOrder === "desc"));
 
         sMessage = this._getText("Ordem Alterada", [this._getText(aStateTextIds[iOrder])]);
         MessageToast.show(sMessage);
       },
 
-      onMessageBindingChange : function (oEvent) {
+      // Manipulação de alterações nas mensagens
+      onMessageBindingChange: function (oEvent) {
         var aContexts = oEvent.getSource().getContexts(),
           aMessages,
           bMessageOpen = false;
-  
+
         if (bMessageOpen || !aContexts.length) {
           return;
         }
-  
+
         // Extract and remove the technical messages
         aMessages = aContexts.map(function (oContext) {
           return oContext.getObject();
         });
         Messaging.removeMessages(aMessages);
-  
+
         this._setUIChanges(true);
         this._bTechnicalErrors = true;
         MessageBox.error(aMessages[0].message, {
-          id : "serviceErrorMessageBox",
-          onClose : function () {
+          id: "serviceErrorMessageBox",
+          onClose: function () {
             bMessageOpen = false;
           }
         });
-  
+
         bMessageOpen = true;
       },
 
+      // Obtenção de texto internacionalizado (i18n)
       _getText: function (sTextId, aArgs) {
         return this.getOwnerComponent().getModel("i18n").getResourceBundle().getText(sTextId, aArgs);
 
       },
 
+      // Configuração de alterações na interface do usuário (UI)
       _setUIChanges: function (bHasUIChanges) {
         if (this._bTechnicalErrors) {
           // If there is currently a technical error, then force 'true'.
@@ -169,7 +339,8 @@ sap.ui.define(
         oModel.setProperty("/hasUIChanges", bHasUIChanges);
       },
 
-      _setBusy : function (bIsBusy) {
+      // Configuração do estado de ocupação (busy)
+      _setBusy: function (bIsBusy) {
         var oModel = this.getView().getModel("appView");
         oModel.setProperty("/busy", bIsBusy);
       }
